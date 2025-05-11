aba858ef3ad0b80197338e85c25334e4a5577845
abdurakhmanov777 <dalgat03@yandex.ru>
2025-05-12 01:19:32 +0300

    Edited

--- index.js
+++ index.js
@@ -4,7 +4,3 @@ const tg = Telegram?.WebApp;
 tg.expand();
 tg.disableVerticalSwipes();
 initializeApp();
-// if (!tg.initDataUnsafe?.user?.id) {
-//     document.body.innerHTML = '<div class="center-message">The site is unavailable outside of Telegram</div>';
-//     return;
-// }
--- utils/animations.js
+++ utils/animations.js
@@ -1,6 +1,19 @@
-export function addAnimation(class_name) {
+// export function addAnimation(class_name) {
+//     const pageElement = document.querySelector(class_name);
+//     pageElement.classList.remove('animate');
+//     void pageElement.offsetWidth;
+//     pageElement.classList.add('animate');
+// }
+
+
+export function addAnimation(class_name, animation_class) {
     const pageElement = document.querySelector(class_name);
-    pageElement.classList.remove('animate');
+    if (!pageElement) return;
+
+    // Удаляем старую анимацию
+    pageElement.classList.remove('animate', 'animate_long');
     void pageElement.offsetWidth;
-    pageElement.classList.add('animate');
+
+    // Добавляем новую
+    pageElement.classList.add(animation_class);
 }
